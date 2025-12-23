1f106c23ec5bc6a5c25e45f7cd99e99b6a413c12
KartoffelL <84570564+KartoffelL@users.noreply.github.com>
2025-12-23 12:30:33 +0100

    Added copyright notices

--- Kartoffel/Licht/Networking/Net.java
+++ Kartoffel/Licht/Networking/Net.java
@@ -35,11 +35,30 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
+
+//
+// Copyright 2025 Kareem Athamneh
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the “Software”), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+// OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 /**
  * Base class of the KLNetworking library, usable for creating clients. (see {@link NetServer}).<br>
  * To create a client, see {@link Net#connect(InetSocketAddress, ClientPeerI, Class, Class)}.
  * 
- * @author Kartoffel Licht
+ * @author Kartoffel Licht<br>
+ * 
  */
 public class Net {
 	
--- Kartoffel/Licht/Networking/NetServer.java
+++ Kartoffel/Licht/Networking/NetServer.java
@@ -44,7 +44,24 @@ import Kartoffel.Licht.Networking.Net.OPCODE;
 import Kartoffel.Licht.Networking.Net.PrimitiveType;
 import Kartoffel.Licht.Networking.Net.ServerPeerI;
 
-//TODO make server also accept HTTP requests (for websockets)
+//
+//Copyright 2025 Kareem Athamneh
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+//and associated documentation files (the “Software”), to deal in the Software without restriction,
+//including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+//subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+//INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+//OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//TODO support newer HTTP protocols
 /**
  * Server implementation for the KLNetworking library. (see {@link Net})<br>
  * To create a server, see {@link #NetServer(InetSocketAddress, Class, Class, ThreadFactory)}
